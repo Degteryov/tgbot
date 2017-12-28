fe769b3463d6d417d2e1daf4671dfa44342de170
Danil <daannill@mail.ru>
2017-12-28 18:40:28 +0300

    two

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 var TelegramBot = require('node-telegram-bot-api');
-
 var token = '519266219:AAGKnHRZzMGyVtUVJjYG0ddHD0Ll_D8pyAQ';
-
 var bot = new TelegramBot(token, {polling: true});
-
 var notes = [];
 bot.onText(/привет!/, function (msg, match) {
     var userId = msg.from.id;
     var text = match[1];
     var time = match[2];
-
+	
     notes.push( { 'uid':userId, 'time':time, 'text':text } );
-
+	
     bot.sendMessage(userId, 'Отлично! Я обязательно напомню, если не сдохну :)');
 	console.log('af');
 });
